9104b57579da254be3c514dd3e85758e0dd31beb
TheGeekMonk <vasusehgal007@gmail.com>
2024-02-03 17:51:21 +0530

    Applayout ready

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom/client";
-
-const AppLayout = () => {
-  return <>This is my Food App - Foodie App</>;
-};
-
-const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
