7d4b901976476c78e98a3bca88ab6451e2e6f7e2
TheGeekMonk <vasusehgal007@gmail.com>
2024-02-04 00:41:13 +0530

    styling changes and about component created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import { Header } from "./Components/Header/Header";
 import { Search } from "./Components/Search/Search";
+import { Card } from "./Components/Card/Card";
+import { Footer } from "./Components/Footer/Footer";
+import { About } from "./Components/About/About";
+
+import "./App.css";
 
 const AppLayout = () => {
   return (
     <>
       <Header />
-      <Search />
+      {/* <Search />
+      <Card />
+      <Footer /> */}
+      <About />
     </>
   );
 };
---
+++ src/Components/About/About.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./About.css";
+import burgerImage from "../../asset/burger.png";
+
+export const About = () => {
+  return (
+    <div className="about-container">
+      <div className="about-text">
+        <h1 className="first-text">Welcome to</h1>
+        <h1 className="first-text">The world of</h1>
+        <div className="second-span">
+          <h1 className="second-text">Tasty & Fresh Food</h1>
+        </div>
+        <h4 className="third-text">
+          "Better you will feel if you eat a{" "}
+          <span className="foodie-text">Foodie App</span> healthy meal"
+        </h4>
+      </div>
+      <div className="burger-img">
+        <img src={burgerImage} alt="Burger" height={250} />
+      </div>
+    </div>
+  );
+};
